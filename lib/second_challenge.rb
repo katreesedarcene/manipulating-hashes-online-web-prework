ef56837268edfdda57d35676e3ee82cb0210c190
Kat B <katreesebarnes@gmail.com>
2019-02-20 20:08:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,6 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
-  
-
+    groceries.each do |name, item|
+    end
 end
